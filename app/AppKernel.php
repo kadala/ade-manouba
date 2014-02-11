b38b0e2596b3556c1dbac79cf1de5a909d6f6143
kadala <kadala@kadala-Inspiron-1545.(none)>
2014-02-11 17:42:50 +0100

    firest commit

---
+++ app/AppKernel.php
@@ -0,0 +1,89 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function init()
+    {
+        // Please read http://symfony.com/doc/2.0/book/installation.html#configuration-and-setup
+        umask(0002);
+
+        parent::init();
+    }
+
+    public function registerBundles()
+    {
+        $bundles = array(
+            // SYMFONY STANDARD EDITION
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+  		
+            // DOCTRINE
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+        	new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+
+            // KNP HELPER BUNDLES
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+
+            // USER
+            new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+            new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+        		
+        	// NEWS
+        	new Sonata\MarkItUpBundle\SonataMarkItUpBundle(),
+        	new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+        	new Sonata\NewsBundle\SonataNewsBundle(),
+        	new Application\Sonata\NewsBundle\ApplicationSonataNewsBundle(),
+        		
+            // MEDIA
+            new Sonata\MediaBundle\SonataMediaBundle(),
+            new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
+            
+        	//Classification
+        	new Sonata\ClassificationBundle\SonataClassificationBundle(),
+        	new Application\Sonata\ClassificationBundle\ApplicationSonataClassificationBundle(),
+        		
+        	//Notification	
+        	new Sonata\NotificationBundle\SonataNotificationBundle(),
+        	new Application\Sonata\NotificationBundle\ApplicationSonataNotificationBundle(),
+
+            // SONATA CORE & HELPER BUNDLES
+        	new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\IntlBundle\SonataIntlBundle(),
+            new Sonata\FormatterBundle\SonataFormatterBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Bazinga\Bundle\FakerBundle\BazingaFakerBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
